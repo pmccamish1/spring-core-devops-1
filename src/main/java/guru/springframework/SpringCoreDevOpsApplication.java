bdc25aca381dc760e57ec2f2947dbc9369088ba0
pmccamish1 <patrick.mccamish@gmx.com>
2018-03-16 11:15:24 -0400

    Fix difference

--- src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
+++ src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
@@ -8,11 +8,5 @@ public class SpringCoreDevOpsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCoreDevOpsApplication.class, args);
-<<<<<<< HEAD
-=======
-
-		//this is a whitespace change test
-		// another test...
->>>>>>> branch 'master' of https://github.com/pmccamish1/spring-core-devops-1.git
 	}
 }
